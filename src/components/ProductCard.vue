f49556afcad87ca0b344a3dfb7eec74d138cf01f
rabia <bayarrabia1@gmail.com>
2023-12-12 20:26:15 +0300

    card component eklendi

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-card class="product-card" @click="redirectToProductDetails">
+    <q-card-section>
+      <h2 class="product-title">{{ product.name }}</h2>
+      <p class="product-subtitle">{{ product.description }}</p>
+    </q-card-section>
+    <q-card-section class="product-image-section">
+      <img :src="product.image" alt="Product Image" class="product-image">
+    </q-card-section>
+    <q-card-section class="buy-now-section">
+      <q-btn color="black" label="Satın Al" @click.stop="buyNow" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+interface Product {
+  name: string;
+  description: string;
+  image: string;
+}
+
+export default defineComponent({
+  props: {
+    product: {
+      type: Object as () => Product,
+      required: true,
+    },
+  },
+
+  methods: {
+    redirectToProductDetails(): void {
+      console.log('Ürün detaylarına yönlendirildi.');
+    },
+    buyNow(): void {
+      console.log('Satın alındı.');
+    },
+  },
+});
+</script>
+
+<style scoped>
+.product-card {
+  max-width: 300px;
+  margin: 10px;
+  background-color: #f5f5f5;
+  cursor: pointer;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.product-title {
+  font-size: 18px;
+  font-weight: bold;
+  color: black;
+  margin-bottom: 5px;
+}
+
+.product-subtitle {
+  font-size: 12px;
+  color: black;
+  margin-bottom: 5px;
+}
+
+.product-image-section {
+  text-align: center;
+  flex-grow: 1;
+}
+
+.product-image {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.buy-now-section {
+  text-align: center;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/pages/ProductList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="product-list-container">
+    <product-card v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ProductCard from 'components/ProductCard.vue';
+
+export default defineComponent({
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Dizüstü Bilgisayarlar',
+          category: 'Gaming Laptop',
+          description: 'Yüksek performanslı ve zarif Dizüstü Bilgisayarları serimizi hemen inceleyin',
+          image: 'https://www.hpstore.com.tr/hp-victus-laptop-16-s0066nt-amd-ryzen-5-7640hs-16gb-ram-512gb-ssd-6gb-geforce-rtx-4050-161-inc-fhd-freedos-mika-gumusu-8v8a0ea-14355-18-K.jpg',
+        },
+        {
+          id: 2,
+          name: 'Masaüstü Bilgisayarlar',
+          category: 'All-in-One PC',
+          description: 'Şık Masaüstü Bilgisayarı serimizi hemen inceleyin',
+          image: 'https://productimages.hepsiburada.net/s/125/550/110000075295322.jpg/format:webp',
+        },
+        {
+          id: 3,
+          name: 'Monitörler',
+          category: 'Monitor',
+          description: 'İster oyun oynayın, ister içerik oluşturun, size uygun her Monitör var',
+          image: 'https://www.hpstore.com.tr/hp-p34hc-g4-34-inc-5ms-hdmi-display-usb-c-4xusb-a-wqhd-curved-led-usb-c-monitor-siyah-21y56as-4956-14-K.jpg',
+        },
+        {
+          id: 4,
+          name: 'İş Bilgisayarları',
+          category: 'Business Laptop',
+          description: 'Gelişmiş güvenlik ve gizlilik özellikleriyle güzelce hazırlanmış iş bilgisayarları',
+          image: 'https://www.hpstore.com.tr/hp-elitebook-640-laptop-intel-core-i7-1255u-16gb-ram-512gb-ssd-intel-iris-xe-graphics-14-inc-fhd-windows-10-pro-gumus-5y4e3ea-12996-16-K.jpg',
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style scoped>
+.product-list-container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
