67682e01bf46173248ce3893e9f9f4d008369582
denizsoylu <110699154+denizsoylu@users.noreply.github.com>
2023-12-13 00:47:08 +0300

    Create ThePage1.vue

---
+++ src/components/ThePage1.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div class="q-pa-md">
+      <q-carousel
+        animated
+        v-model="slide"
+        arrows
+        navigation
+        infinite
+      >
+        <q-carousel-slide :name="1" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/homepage/slider/2023/12/hp-store-gunleri-omen-oyuncu-bilgisayarlari.jpg" />
+        <q-carousel-slide :name="2" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/kampanya/yazicimi-yenile/yazicimi-yenile.jpg" />
+        <q-carousel-slide :name="3" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/homepage/slider/2023/09/15/hp-store-maximum-vade-farksiz-3-taksit-firsati.jpg" />
+        <q-carousel-slide :name="4" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/homepage/slider/2023/10/hp-store-oyun-tutkunlarinin-secimi.jpg" />
+        <q-carousel-slide :name="5" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/homepage/slider/20220902/yeni-hp-laserjet-tank-tasarruflu.jpg?v=1" />
+        <q-carousel-slide :name="6" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/homepage/slider/20220531/hp-baski-dunyasi.jpg" />
+        <q-carousel-slide :name="7" img-src="https://www.hpstore.com.tr/Data/EditorFiles/rdm/homepage/slider/20220531/hp-smart-tank-kartussuz-murekkep-tankli-yazici.jpg" />
+      </q-carousel>
+    </div>
+  </template>
+
+  <script>
+  import { ref } from 'vue'
+  
+  export default {
+    setup () {
+      return {
+        slide: ref(1)
+      }
+    }
+  }
+  </script>
+  
