f8a38d409ffd3d50afddabce9cf8f44a417eb2ac
rabia <bayarrabia1@gmail.com>
2023-12-12 20:10:52 +0300

    sepet component eklendi

---
+++ src/components/TheSepet.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-dialog v-model="sepetIletisiGoster" persistent>
+    <q-card>
+      <q-card-section>
+        <div class="q-pa-md" style="display: flex; align-items: center;">
+          <div style="flex: 1;">
+            <img
+              src="https://www.hpstore.com.tr/hp-victus-laptop-16-r0038nt-intel-core-i7-13700h-16gb-ram-512gb-ssd-nvidia-geforce-rtx-4050-6gb-161-inc-fhd-freedos-mavi-7p6l4ea-14212-18-K.jpg"
+              alt="Product Image"
+              style="max-width: 100%; height: auto;"
+            />
+          </div>
+          <div style="flex: 2; padding-left: 16px;">
+            <q-btn
+              class="q-pa-xs"
+              round
+              flat
+              dense
+              icon="close"
+              @click="sepetIletisiGoster = false"
+              style="position: absolute; top: 0; right: 0; color: #1976D2;"
+            />
+            <div class="text-h6" style="background-color: #1976D2; color: white; padding: 8px; width: 100%; box-sizing: border-box;">
+              ÜRÜN SEPETİNİZE EKLENDİ
+            </div>
+            <div class="text-body1">
+              HP Victus Laptop 16-S0065NT AMD Ryzen 7-7840HS 32GB RAM 1TB SSD 8GB GeForce RTX 4070 16.1 Inç FHD FreeDOS Mavi
+            </div>
+            <div class="text-h6">47.999,00 TL</div>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions align="right" style="justify-content: space-between;">
+        <q-btn label="ALIŞVERİŞE DEVAM" color="primary" @click="alisveriseDevam"></q-btn>
+        <q-btn label="Sepete Git" color="primary" @click="sepeteGit"></q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      sepetIletisiGoster: false,
+    };
+  },
+  methods: {
+    alisveriseDevam(): void {
+      this.sepetIletisiGoster = false; 
+    },
+    sepeteGit(): void {
+      this.sepetIletisiGoster = false;
+    },
+    showSepetIletisi(): void {
+      this.sepetIletisiGoster = true;
+    },
+  },
+});
+</script>
+
+<style scoped>
+/* Stillemeler */
+</style>
+
+
+
---
+++ src/pages/SepetSayfasi.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page>
+    <!-- Sayfa içeriği -->
+    <TheSepet ref="sepetComponent"></TheSepet>
+
+    <!-- Kullanıcı bir eylem gerçekleştirdiğinde (örneğin bir butona tıkladığında) -->
+    <q-btn @click="showSepet">Sepeti Göster</q-btn>
+  </q-page>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheSepet from 'components/TheSepet.vue';
+
+export default defineComponent({
+  methods: {
+    showSepet() {
+      // TheSepet.vue componentini çağır
+      (this.$refs.sepetComponent as typeof TheSepet).showSepetIletisi();
+    },
+  },
+  components: {
+    TheSepet,
+  },
+});
+</script>
+
+  
+  
