1023415179983dc678b1dd2d9cf7aed704fb3805
bayarrabia <bayarrabia1@gmail.com>
2024-01-22 16:47:53 +0300

    comp12

---
+++ src/pages/ThePage12.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page>
+    <!-- Sayfa içeriği -->
+    <ThePage12></ThePage12>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import ThePage12 from 'components/TheComp12.vue';
+</script>
